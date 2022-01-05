649dd025e288a77a6ee1ab6d02953a2ee245bb7d
Riza Agustin <rizaagustin20@gmail.com>
2022-01-05 15:46:01 +0700

    memanipulasi properties

---
+++ Properties.php
@@ -0,0 +1,22 @@
+<?php
+require_once "data/Person.php";
+
+$person = new Person();
+$person->name = "Riza";
+$person->address = "Jakarta";
+$person->country= "Indonesia";
+var_dump($person);
+
+echo "Name : $person->name ". PHP_EOL;
+echo "Address : $person->address ". PHP_EOL;
+echo "Country : $person->country ". PHP_EOL;
+
+$person2 = new Person();
+$person2->name = "Arief";
+$person2->address = "Jakarta";
+$person2->country= "Indonesia";
+var_dump($person2);
+
+echo "Name : $person2->name ". PHP_EOL;
+echo "Address : $person2->address ". PHP_EOL;
+echo "Country : $person2->country ". PHP_EOL;
