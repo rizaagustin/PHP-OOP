9e6a98204119064edac02fe63efacd2932b113ad
Riza Agustin <rizaagustin20@gmail.com>
2022-01-05 22:39:44 +0700

    Membuat Function

--- Properties.php
+++ Properties.php
@@ -4,7 +4,7 @@ require_once "data/Person.php";
 $person = new Person();
 $person->name = "Riza";
 $person->address = "Jakarta";
-$person->country= "Indonesia";
+// $person->country= "Indonesia";
 var_dump($person);
 
 echo "Name : $person->name ". PHP_EOL;
@@ -14,9 +14,10 @@ echo "Country : $person->country ". PHP_EOL;
 $person2 = new Person();
 $person2->name = "Arief";
 $person2->address = "Jakarta";
-$person2->country= "Indonesia";
-var_dump($person2);
+$person2->country= "Thailand";
+var_dump($person2); 
 
 echo "Name : $person2->name ". PHP_EOL;
 echo "Address : $person2->address ". PHP_EOL;
 echo "Country : $person2->country ". PHP_EOL;
+
--- data/Person.php
+++ data/Person.php
@@ -1,4 +1,16 @@
 <?php
 class Person {
+  var $name; //properties
+  var $address;
+  var $country = "Indonesia"; //default value
+
+// hanya bisa di php 7.4 keatas saat ini kita pakai 7.2
+//   var string $name; //properties
+//   var ?string $address = null; // addres bole null atau nullable properties
+//   var string $country;
+
+    function sayHello(string $name){
+        echo "Hallo $name". PHP_EOL;
+    }
 
 }
