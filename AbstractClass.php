c741e8cf60dcd76349edf8770287d7b02e8c654f
Riza Agustin <rizaagustin20@gmail.com>
2022-01-10 00:37:57 +0700

    Abstract Class

---
+++ AbstractClass.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "data/Location.php";
+
+use Data\{Location, City, Province, Country};
+
+// $location = new Location(); //object error
+$city = new City();
+$province = new Province();
+$country = new Country();
