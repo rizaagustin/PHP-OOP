f4d629b103e8ab50a65539b7ce1c737df7171da6
Riza Agustin <rizaagustin20@gmail.com>
2022-01-13 00:13:38 +0700

    final function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -7,11 +7,18 @@ class SocialMedia
 
 final class Facebook extends SocialMedia
 {
-
+    final public function login(string $username, string $password): bool
+    {
+        return true;
+    }
 }
 
 // error karena class facebook final
 class FakeFacebook extends SocialMedia
 {
-
+    // error tidak bisa di override kerena diatas sudah final
+    public function login(string $username, string $password): bool
+    {
+        return false;
+    }
 }
